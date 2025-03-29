01496cceaba9f7d1a86f29e95b166f3398bcdf7e
Sumit Gusain <sumitgusain5311@gmail.com>
2025-03-30 03:02:03 +0530

    created double horizontal slider and animated the circle using framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import Landing from '../components/Landing'
 import Projects from '../components/Projects/Projects'
 import Description from '../components/Description'
+import SlidingImages from '../components/SlidingImages'
 export default function Home() {
   useEffect( () => {
     (
@@ -22,8 +23,8 @@ export default function Home() {
       <Landing />
       <Description />
       <Projects />
-      {/* <SlidingImages />
-      <Contact /> */}
+      <SlidingImages />
+      {/* <Contact /> */}
     </main>
   )
 }
