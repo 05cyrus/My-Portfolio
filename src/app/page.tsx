81565c608d993b997cdef816a49d6eece7e0562f
Sumit Gusain <sumitgusain5311@gmail.com>
2025-04-01 04:31:29 +0530

    fix the error related to key-index in slidingimages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,37 @@
 'use client';
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 import Landing from '../components/Landing'
 import Projects from '../components/Projects/Projects'
 import Description from '../components/Description'
 import SlidingImages from '../components/SlidingImages'
 import Contact from '../components/Contact'
+import Preloader from '../components/Preloader'
+import { AnimatePresence } from 'framer-motion'
+
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect( () => {
     (
       async () => {
           const LocomotiveScroll = (await import('locomotive-scroll')).default
           const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
       }
     )()
   }, [])
 
-
   return (
-    <main>
-      {/* <AnimatePresence mode='wait'>
+    <main >
+      <AnimatePresence mode='wait'>
         {isLoading && <Preloader />}
-      </AnimatePresence> */}
+      </AnimatePresence>
       <Landing />
       <Description />
       <Projects />
